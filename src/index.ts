77b2fcf11e9a2f37b3b08a0d7db03a5b9067e5ad
hyeonQyu <dhk0561@naver.com>
2022-05-02 11:37:38 +0900

    chore: 프로젝트 기본 설정

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+app.use(cors());
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+        limit: '50mb',
+    }),
+);
+app.use(bodyParser.json());
+
+app.listen(6200, () => {
+    console.log('express server started with port 6200');
+});

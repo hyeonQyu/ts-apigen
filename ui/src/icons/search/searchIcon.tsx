58f03e10ca268a49d20d566d65e772d2c93ee541
hyeonQyu <dhk0561@naver.com>
2022-05-16 17:45:52 +0900

    feat: searchIcon 생성

---
+++ ui/src/icons/search/searchIcon.tsx
@@ -0,0 +1,45 @@
+import { IconCommonProps } from '@defines/iconCommonProps';
+
+export interface SearchIconProps extends Pick<IconCommonProps, 'width' | 'height' | 'opacity'> {}
+
+function ArrowIcon(props: SearchIconProps) {
+    const { width = 16, height = 10, opacity = 0 } = props;
+
+    return (
+        <>
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20">
+                {/*<defs>*/}
+                {/*    <style>.a{opacity:0.3;}.b{fill:#fff;opacity:0;}.c{fill:#6a7684;}</style>*/}
+                {/*</defs>*/}
+                <g className="a">
+                    <rect className="b" width="20" height="20" transform="translate(20 20) rotate(180)" />
+                    <path
+                        className="c"
+                        d="M10.791,11.878,11.54,12.9a3.019,3.019,0,0,0,.3.354l3.071,3.015a.5.5,0,0,0,.71,0l.7-.7a.5.5,0,0,0,0-.71L13.3,11.868a2.981,2.981,0,0,0-.429-.356l-1.011-.69"
+                        transform="translate(1 1)"
+                    />
+                    <path
+                        className="c"
+                        d="M7.5,2A5.5,5.5,0,1,0,13,7.5,5.5,5.5,0,0,0,7.5,2Zm0,9A3.5,3.5,0,1,1,11,7.5,3.5,3.5,0,0,1,7.5,11Z"
+                        transform="translate(1 1)"
+                    />
+                </g>
+            </svg>
+
+            <style jsx>{`
+                a {
+                    opacity: 0.3;
+                }
+                b {
+                    fill: #fff;
+                    opacity: 0;
+                }
+                c {
+                    fill: #6a7684;
+                }
+            `}</style>
+        </>
+    );
+}
+
+export default ArrowIcon;
